ca5fa5eddfcf5181df2b2f0153810fb781319a0a
Juan CV <jacklazerjscv@gmail.com>
2023-09-24 23:29:45 -0500

    Gramatica BNF de expresion-FNC

--- ejercicio1.rkt
+++ ejercicio1.rkt
@@ -4,3 +4,16 @@
 ; Juan Sebastian Cifuentes Vallejo - 202179800
 ; Maria Alejandra Carvajal Perez - 202178495
 ; Yissy Katherine Posso Perea - 202181910
+
+; Gramatica BNF:
+;
+; <expresion-FNC> ::= FNC <entero-positivo> (<conjuncion-de-clausulas>)
+;
+; <conjuncion-de-clausulas> ::= (<clausula>)
+;                           ::= (<clausula>) and <conjuncion-de-clausulas>
+;
+; <clausula> ::= <variable>
+;            ::= <variable> or <clausula>
+;
+; <variable> ::= <entero-positivo>
+;            ::= -<entero-positivo>
